4cf6692458cfdaff9b8db17efea54bbd2fdf1535
Forte <33382293+TheDreigon@users.noreply.github.com>
2024-08-11 22:06:35 +0100

    work on Route 2's visual.

--- data/maps/objects/Route2.asm
+++ data/maps/objects/Route2.asm
@@ -15,18 +15,19 @@ Route2_Object:
 	warp_event 17, 35, ROUTE_2_GATE, 2
 
 	def_bg_events
-	bg_event  5, 65, TEXT_ROUTE2_SIGN
+	bg_event  7, 65, TEXT_ROUTE2_SIGN
 	bg_event 11, 11, TEXT_ROUTE2_DIGLETTS_CAVE_SIGN
 
 	def_object_events
-	object_event 13, 54, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE2_MOON_STONE, MOON_STONE
-	object_event 13, 45, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE2_HP_UP, HP_UP
+	object_event 19,  2, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE2_HP_UP, HP_UP
+	object_event 18, 64, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE2_RARE_CANDY, RARE_CANDY
+	object_event 18, 68, SPRITE_POKE_BALL, STAY, NONE, TEXT_ROUTE2_MOON_STONE, MOON_STONE
 
 	def_warps_to ROUTE_2
 
 	; unused
-	warp_to 2, 7, 4
+	warp_to  0,  4, 4
 	db $12, $c7, $9, $7
-	warp_to 2, 7, 4
-	warp_to 2, 7, 4
-	warp_to 2, 7, 4
+	warp_to  0,  4, 4
+	warp_to  0,  4, 4
+	warp_to  0,  4, 4
